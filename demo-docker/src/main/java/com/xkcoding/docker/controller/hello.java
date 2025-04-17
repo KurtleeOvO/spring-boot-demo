c2cb3c067e38f6a75516d5e29c533b6be4f170b7
kurt <1932758175@qq.com>
2025-04-17 13:53:18 +0800

    add src/main/java/com/xkcoding/docker/controller/hello.java

--- demo-docker/src/main/java/com/xkcoding/docker/controller/hello.java
+++ demo-docker/src/main/java/com/xkcoding/docker/controller/hello.java
@@ -3,5 +3,7 @@ package com.xkcoding.docker.controller;
 public class hello {
   public static void main(String[] args) {
     System.out.println("hello");
+    System.out.println("1");
+    System.out.println("2");
   }
 }
